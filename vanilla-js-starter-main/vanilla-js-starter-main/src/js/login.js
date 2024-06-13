ed8b5c29f4fbf7686f8d5924be2f9dbf8d97be9a
W-Alexander <alexanderaguilar726@gmail.com>
2024-06-13 08:12:51 -0600

    Validación para negar el paso al mismo usuario encontrado

---
+++ vanilla-js-starter-main/vanilla-js-starter-main/src/js/login.js
@@ -0,0 +1,8 @@
+import { getData, postData } from "./fetch.js";
+let correo = document.getElementById("correo");
+let contraseña = document.getElementById("contraseña");
+let entrar = document.getElementById("entrar");
+
+
+
+
